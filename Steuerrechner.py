1cf7cf4451a8a6209e8b33f44b2e55b56fa5478d
philippbuling <philipp.buling@icloud.com>
2022-12-01 23:08:01 +0100

    V0.1

--- Steuerrechner.py
+++ Steuerrechner.py
@@ -1,14 +1,21 @@
+print("***** Berechnung der Mehrwertsteuer *****")
+
 while True:
     try:
         preis = input("Preis: ")
+        if not preis:
+            print("Auf wiedersehen!")
+            break
         steuersatz = input("Steuersatz: ")
-        if not preis or not steuersatz:
+        if not steuersatz:
+            print("Auf wiedersehen!")
             break
         steuer = float(preis) * float(steuersatz) / 100
         gesamtpreis = float(preis) + float(steuer)
         print("Steuersatz: ", steuersatz, "%")
         print("Steuern: ", steuer, "EUR")
         print("Gesamtpreis: ", gesamtpreis, "EUR")
+        print(20*"*")
     except ValueError:
         print("Bitte geben Sie eine Zahl ein!")
         print("Beispiel: 100")

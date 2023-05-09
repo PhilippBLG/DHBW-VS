6742ec0f4fc393c3313bd24b476a77c8492ecfda
philippbuling <philipp.buling@icloud.com>
2023-05-09 23:26:06 +0200

    V0.1

---
+++ Common Sense Guide/Tasks.py
@@ -0,0 +1,31 @@
+def find_ducpliates(array):
+    doubles = {}
+    for letter in array:
+        if doubles.get(letter):
+            return letter
+        doubles[letter] = True
+    return None
+
+def missing_alphabet(string):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for letter in alphabet:
+        if letter not in string:
+            return letter
+    return None
+
+def first_nondouble(string):
+    doubles = {}
+    for letter in string:
+        doubles[letter] = 0
+    for letter in string:
+        doubles[letter] += 1
+    for letter in doubles:
+        if doubles.get(letter) == 1:
+            return letter
+
+
+print(find_ducpliates(["a", "b", "c", "d", "c", "e", "f"]))
+print(missing_alphabet("the quick brown box jumps over a lazy dog"))
+print(first_nondouble("minimum"))
+
+

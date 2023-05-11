2ee6db5bdb2c0c7a1cc1777d4274765f59f301ff
philippbuling <philipp.buling@icloud.com>
2023-05-11 11:11:18 +0200

    V0.2

--- Sortieren/Insertion_sort.py
+++ Sortieren/Insertion_sort.py
@@ -1,16 +1,18 @@
 def sort(a, size):
+    vergleiche = 0
+    vertauschungen = 0
     for i in range(1, size):
-        if a[i] < a[i - 1]:
-            temp = a[i-1]
-            j = i
-            while a[j] < a[j-1] and j > 0:
-                a[j-1] = a[j]
-                a[j] = temp
-                j -= 1
-                temp = a[j-1]
-
-
-    return a
+        temp = a[i-1]
+        j = i
+        while a[j] < a[j-1] and j > 0:
+            vergleiche += 1
+            vertauschungen += 1
+            a[j-1] = a[j]
+            a[j] = temp
+            j -= 1
+            temp = a[j-1]
+        vergleiche += 1
+    return vergleiche, vertauschungen, a
 
 
 print(sort([61, 42, 59, 14, 23, 79, 37, 92], len([61, 42, 59, 14, 23, 79, 37, 92])))
